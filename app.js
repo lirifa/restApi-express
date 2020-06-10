77651142d93cba06ef397faff5d38ee65ceba01e
rafa <rafa_im@163.com>
2020-06-10 18:19:21 +0800

    添加七牛云sdk

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var merchantRouter = require('./routes/merchant');
+var qnRouter = require('./routes/qiniu');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/merchant', merchantRouter);
+app.use('/qn', qnRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/qiniu.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var qiniu = require('qiniu');
+var AccessKey= '1bRwfIFdhwLdhNCdGuQaSAFM3Mq6fR1q6LNfIBt2';
+var SecretKey= 'UZLdH-U_SHY1nYjjbR9-f3VkKvsw_MrPV-X73mZP';
+
+
+/* GET home page. */
+router.get('/token', function(req, res, next) {
+    var mac = new qiniu.auth.digest.Mac(AccessKey, SecretKey);
+    var options = {
+        scope: 'rafa-test',
+    };
+    var putPolicy = new qiniu.rs.PutPolicy(options);
+    var uploadToken = putPolicy.uploadToken(mac);
+    var data = {
+        code: 200,
+        data: {
+            token: uploadToken
+        }
+    };
+    res.send(data);
+});
+
+module.exports = router;
