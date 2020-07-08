4fc87311c5ca2ba5a1e36cb80f6c509dc397c41a
rafa <rafa_im@163.com>
2020-07-08 14:32:05 +0800

    允许跨域请求

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // 设置允许跨域
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); //访问控制允许来源：所有
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); //访问控制允许报头 X-Requested-With: xhr请求
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild'); //访问控制允许报头 X-Requested-With: xhr请求
     res.header('Access-Control-Allow-Metheds', 'PUT, POST, GET, DELETE, OPTIONS'); //访问控制允许方法
     res.header('X-Powered-By', 'nodejs'); //自定义头信息，表示服务端用nodejs
     res.header('Content-Type', 'application/json;charset=utf-8');
--- controllers/user.js
+++ controllers/user.js
@@ -1,62 +1,62 @@
 class User {
-    constructor() {
-        this.db = require('../dao/connect');
-        this.Token = require('../utils/token');
-    }
+  constructor() {
+    this.db = require('../dao/connect');
+    this.Token = require('../utils/token');
+  }
 
-    register(json) {
-        return new Promise((resolve, reject) => {
-            console.log(json);
-            if(json.permission_role === '1') {
-                this.db.select(['name'], 'user', { name: json.data.name }).then(result => {
-                    if (result.length === 0) {
-                        this.db.insert(json.data, 'user').then(res => {
-                            resolve({code: 200, msg: '用户注册成功'});
-                        }).catch(err => {
-                            reject({code: -1, msg: '操作数据库失败'});
-                        });
-                    } else {
-                        reject({code: -1, msg: '已经存在此账号'});
-                    }
-                });
-            } else {
-                reject({code: -1, msg: '权限不足'});
-            }
-
-        });
-    }
-
-    login(json) {
-        return new Promise((resolve, reject) => {
-            this.db.select(['id','name', 'password', 'permission_role'], 'user', { name: json.name }).then(result => {
-                if (result.length !== 0) {
-                    if(result[0].password === json.password) {
-                        const token = this.Token.encrypt({id:result[0].id, permission_role: result[0].permission_role}, '15d');
-                        resolve({code: 200, msg: '登录成功', data: result[0], token: token})
-                    } else {
-                        reject({code: -1, msg: '密码错误'})
-                    }
-                } else {
-                    reject({code: -1, msg: '用户不存在'});
-                }
-            });
-        });
-    }
-
-    info(id){
-        return new Promise((resolve, reject) => {
-            this.db.select(['id','name','permission_role'], 'user', { id: id }).then(result => {
-                if (result.length !== 0) {
-                    const token = this.Token.encrypt({id:result[0].id, permission_role: result[0].permission_role}, '15d');
-                    resolve({code: 200, msg: '操作成功', data: result[0], token: token})
-                } else {
-                    reject({code: -1, msg: '未找到用户信息'});
-                }
+  register(json) {
+    return new Promise((resolve, reject) => {
+      console.log(json);
+      if (json.permission_role === '1') {
+        this.db.select(['name'], 'user', {name: json.data.name}).then(result => {
+          if (result.length === 0) {
+            this.db.insert(json.data, 'user').then(res => {
+              resolve({code: 200, msg: '用户注册成功'});
             }).catch(err => {
-                reject({code: -1, msg: '操作数据库失败'});
+              reject({code: -1, msg: '操作数据库失败'});
             });
+          } else {
+            reject({code: -1, msg: '已经存在此账号'});
+          }
         });
-    }
+      } else {
+        reject({code: -1, msg: '权限不足'});
+      }
+
+    });
+  }
+
+  login(json) {
+    return new Promise((resolve, reject) => {
+      this.db.select(['id', 'name', 'password', 'permission_role'], 'user', {name: json.name}).then(result => {
+        if (result.length !== 0) {
+          if (result[0].password === json.password) {
+            const token = this.Token.encrypt({id: result[0].id, permission_role: result[0].permission_role}, '15d');
+            resolve({code: 200, msg: '登录成功', data: result[0], token: token})
+          } else {
+            reject({code: -1, msg: '密码错误'})
+          }
+        } else {
+          reject({code: -1, msg: '用户不存在'});
+        }
+      });
+    });
+  }
+
+  info(id) {
+    return new Promise((resolve, reject) => {
+      this.db.select(['id', 'name', 'permission_role'], 'user', {id: id}).then(result => {
+        if (result.length !== 0) {
+          const token = this.Token.encrypt({id: result[0].id, permission_role: result[0].permission_role}, '15d');
+          resolve({code: 200, msg: '操作成功', data: result[0], token: token})
+        } else {
+          reject({code: -1, msg: '未找到用户信息'});
+        }
+      }).catch(err => {
+        reject({code: -1, msg: '操作数据库失败'});
+      });
+    });
+  }
 
 }
 
