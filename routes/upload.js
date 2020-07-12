6fac0ca1ef63720aefe41116209cfbe682765641
rafa <rafa_im@163.com>
2020-07-12 21:14:21 +0800

    ...

--- routes/upload.js
+++ routes/upload.js
@@ -9,6 +9,7 @@ const upload = require('../controllers/upload');
 router.post('/img', verifyToken, upload.array('img',5), (req, res) => {
     // 读取上传的图片信息
     var files = req.files;
+    console.log(files);
     // 设置返回结果
     var result = {};
     if(!files[0]) {
